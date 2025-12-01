c79a448b236a583d218dbb7853705578128e8860
finn <finnw16@nycstudents.net>
2025-12-01 14:08:59 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,2 +1,15 @@
 print("Hello World!")
 
+print("(Name) is a (adjective1) (noun1). I hope that they live a (adjective2) life, where they can (verb) as much as they want. Every time we are together, they make me feel (Emotion). My favorite memory is when we went to (location), (PastTenseVerb), and ate (food).")
+
+(Name) = input()
+(adjective1) = input()
+(noun1) = input()
+(adjective2) = input()
+(verb) = input()
+(Emotion) = input()
+(location) = input()
+(PastTenseVerb) = input()
+(food) = input()
+
+print(f"{(Name)} is a {(adjective1)} {(noun1)}. I hope that they live a {(adjective2)} life, where they can {(verb)} as much as they want. Every time we are together, they make me feel {(Emotion)}. My favorite memory is when we went to {(location)}, {(PastTenseVerb)}, and ate {(food)}.")
