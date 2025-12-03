4520b9542c151d11c85a7344a92eca9d58475729
finn <finnw16@nycstudents.net>
2025-12-03 09:16:55 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,15 +1,21 @@
+import random
 print("Hello World!")
 
-print("(Name) is a (adjective1) (noun1). I hope that they live a (adjective2) life, where they can (verb) as much as they want. Every time we are together, they make me feel (Emotion). My favorite memory is when we went to (location), (PastTenseVerb), and ate (food).")
+story = random.randint(1, 2)
 
-(Name) = input()
-(adjective1) = input()
-(noun1) = input()
-(adjective2) = input()
-(verb) = input()
-(Emotion) = input()
-(location) = input()
-(PastTenseVerb) = input()
-(food) = input()
+(Name) = input("name: ")
+(adjective1) = input("adjective:")
+(noun1) = input("noun: ")
+(adjective2) = input("another adjective: ")
+(verb) = input("verb: ")
+(Emotion) = input("emotion: ")
+(location) = input("location:")
+(PastTenseVerb) = input("Past tense verb: ")
+(food) = input("food: ")
+(Color) = input("color: ")
 
-print(f"{(Name)} is a {(adjective1)} {(noun1)}. I hope that they live a {(adjective2)} life, where they can {(verb)} as much as they want. Every time we are together, they make me feel {(Emotion)}. My favorite memory is when we went to {(location)}, {(PastTenseVerb)}, and ate {(food)}.")
+if story == 1:
+	print(f" Dear Professor {(Name)}: I understand that I recived a D- on my paper about {(noun1)}s- However, I hope that I can remedy this with an explanation. My {(adjective1)} mother was {(verb)}ing nonstop at the time, and I was understandably {(adjective2)} about it. To make matters worse, I was away in {(location)} and had just {(PastTenseVerb)}. I hope you can feel {(Emotion)} for me at this time, as I humbly request an extension. PS: as for the {(food)} stain, that one's my bad. sorry. ")
+
+if story == 2:
+	print(f"LOST DOG: {(Name)}. My dog is {(Color)}, {(adjective1)}, and very, very {(adjective2)}. She likes to {(verb)} and eat {(food)}. The last time I saw her we were at {(location)}. Please help me find her! ")
