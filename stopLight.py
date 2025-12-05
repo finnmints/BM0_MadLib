a77826d72267df312e897130f82f00e889fd123d
finn <finnw16@nycstudents.net>
2025-12-05 12:41:37 -0500

    Update stopLight.py

--- stopLight.py
+++ stopLight.py
@@ -4,21 +4,26 @@ from tkmacosx import Button
 
 # Create the main window
 root = Tk()
-root.title("Enter Title Here")
+root.title("StopLight")
 
 #Set size of window
 root.geometry("300x150")
 
 # Create buttons
 red_button = Button(root, text="Red", background='red')
+yellow_button = Button(root, text="Yellow", background='yellow')
+green_button = Button(root, text="Green", background='green')
 
 #Add a label
-label = Label(root, text="CHANGE ME!")
+label = Label(root, text="This is a stoplight")
 
 # Place widgets in window (with pack function!)
 label.pack()
 red_button.pack()
-
+yellow_button.pack()
+green_button.pack()
 
 # Start the GUI event loop
 root.mainloop()
+
+message = Message("This is a stoplight", "Hi Mr. O")
